fa77e997e3fec513cf55416c9f21ce57abcdece4
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-02-04 00:32:15 +0100

    fixed lib object path

--- lib_lettering.php
+++ lib_lettering.php
@@ -4,7 +4,7 @@ class lib_lettering extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'lettering/lib/jquery.lettering.js');}
+        return new \SYSTEM\PLIB('lettering/lib/jquery.lettering.js');}
     public static function version(){
         return '<a href="https://github.com/davatron5000/Lettering.js" target="_blank">https://github.com/davatron5000/Lettering.js</a> (commit: 48388649e0)';}
 }
